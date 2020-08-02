d67472bd66963d2921e00dd8ecb794cebe6d046d
shokohsc <shokohsc@gmail.com>
2020-08-03 00:30:41 +0200

    alpine:latest base

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM centos
-RUN yum -y install /usr/bin/ps nfs-utils && yum clean all
-RUN mkdir -p /exports
+FROM alpine:latest
+
+RUN apk add --no-cache --update nfs-utils
+
 ADD run_nfs.sh /usr/local/bin/
 
-# expose mountd 20048/tcp and nfsd 2049/tcp and rpcbind 111/tcp
-EXPOSE 2049/tcp 20048/tcp 111/tcp 111/udp
-
 ENTRYPOINT ["/usr/local/bin/run_nfs.sh"]
+
+EXPOSE 2049/tcp
--- run_nfs.sh
+++ run_nfs.sh
@@ -2,33 +2,18 @@
 
 function start()
 {
+    echo "$@    *(fsid=0,rw,sync,nohide,no_root_squash,no_subtree_check,insecure)" > /etc/exports
+    cat /etc/exports
+    ls -lah $@
 
-    # prepare /etc/exports
-    echo "/exports *(rw,fsid=0,insecure,no_root_squash)" > /etc/exports
-    for i in "$@"; do
-        # fsid=0: needed for NFSv4
-        echo "$i *(rw,insecure,no_root_squash)" >> /etc/exports
-        echo "Serving $i"
-    done
+    /usr/sbin/exportfs -rv
+    /usr/sbin/exportfs -s
+    /usr/sbin/exportfs
 
-    # start rpcbind if it is not started yet
-    /usr/sbin/rpcinfo 127.0.0.1 > /dev/null; s=$?
-    if [ $s -ne 0 ]; then
-       echo "Starting rpcbind"
-       /usr/sbin/rpcbind -w
-    fi
+    /usr/sbin/rpc.nfsd --debug --port 2049 --no-nfs-version 2 --no-nfs-version 3 --no-udp
+    cat /proc/fs/nfsd/versions
 
-    mount -t nfsd nfds /proc/fs/nfsd
-
-    # -N 4.x: disable NFSv4
-    # -V 3: enable NFSv3
-    /usr/sbin/rpc.mountd -N 2 -V 3 -V 4 -V 4.1
-
-    /usr/sbin/exportfs -r
-    # -G 10 to reduce grace time to 10 seconds (the lowest allowed)
-    /usr/sbin/rpc.nfsd -G 10 -N 2 -V 3 -V 4 -V 4.1 2
-    /usr/sbin/rpc.statd --no-notify
-    echo "NFS started"
+    /usr/sbin/rpc.mountd --debug all --no-nfs-version 2 --no-nfs-version 3 --no-udp
 }
 
 function stop()
@@ -36,7 +21,7 @@ function stop()
     echo "Stopping NFS"
 
     /usr/sbin/rpc.nfsd 0
-    /usr/sbin/exportfs -au
+    /usr/sbin/exportfs -auv
     /usr/sbin/exportfs -f
 
     kill $( pidof rpc.mountd )
